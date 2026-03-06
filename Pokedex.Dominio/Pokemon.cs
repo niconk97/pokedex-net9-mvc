031f3290ce352e5a64536049bcc988db38883c4d
niconk97 <nicolasnk16@gmail.com>
2026-03-06 16:46:49 -0300

    Agrego validacion en nombre del pokemon; Agrego vista y metodo para dar de alta un Pokemon

--- Pokedex.Dominio/Pokemon.cs
+++ Pokedex.Dominio/Pokemon.cs
@@ -1,14 +1,21 @@
 ﻿namespace Pokedex.Dominio;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 public class Pokemon
 {
     public int Id { get; set; }
+    
     [DisplayName("Número")]
     public int Numero { get; set; }
+
+    [Required(ErrorMessage = "El Nombre es requerido")]
     public string Nombre { get; set; }
+    
     [DisplayName("Descripción")]
     public string Descripcion { get; set; }
+
+    [DisplayName("Imagen")]
     public string UrlImagen { get; set; }
     public Elemento Tipo { get; set; }
     public Elemento Debilidad { get; set; }
